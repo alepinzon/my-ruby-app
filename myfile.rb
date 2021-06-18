dc459518dd38bdcf080397ee440b6da9b7701c56
alepinzon <alepinzon@me.com>
2021-06-18 15:47:38 -0600

    Automatically fix Octo RuboCop notices.

--- myfile.rb
+++ myfile.rb
@@ -1,16 +1,16 @@
 class Octocat
-    def initialize(name, *breeds)
-      # Instance variables
-      @name = name
-      @breeds = breeds
-    end
-  
-    def display
-      breed = @breeds.join("-")
-  
-      puts "I am of #{breed} breed, and my name is #{@name}."
-    end
+  def initialize(name, *breeds)
+    # Instance variables
+    @name = name
+    @breeds = breeds
   end
-  
-  m = Octocat.new("Mona", "cat", "octopus")
-  m.display
+
+  def display
+    breed = @breeds.join('-')
+
+    puts "I am of #{breed} breed, and my name is #{@name}."
+  end
+  end
+
+m = Octocat.new('Mona', 'cat', 'octopus')
+m.display
